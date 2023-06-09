199e87bb90565d8f9c112c49bf483977dc25e56f
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-08 18:41:44 -0700

    todo.store.js State y filtros agregados

---
+++ src/store/todo.store.js
@@ -0,0 +1,18 @@
+import { Todo } from '../todos/models/todo.model';
+
+const Filters = {
+    All: 'all',
+    Completed: 'completed',
+    Pending: 'pending',
+}
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del infinito'),
+        new Todo('Piedra del tiempo'),
+
+    ],
+    
+    filter: Filters.All,
+}
