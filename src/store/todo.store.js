e27e3c6070c067cb523ed9d05c659c06947ff048
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-09 13:59:09 -0700

    todo.store.js Local storage agregado

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,13 +20,24 @@ const state = {
 }
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
     console.log('InitStore 🥑')
 
 }
 
 const loadStore = () => {
-    throw new Error('Not implemented');
+   if( !localStorage.getItem('state')) return;
+
+   const { todos = [], filter = Filters.All } = JSON.parse(localStorage.getItem('state'));
+   state.todos = todos;
+   state.filter = filter;
+
+   
+}
+
+const saveStateToLocalStorage = () => {
+  
+    localStorage.setItem('state', JSON.stringify(state));
 
 }
 
@@ -55,6 +66,7 @@ const addTodo = ( description ) => {
     if( !description ) throw new Error('Description is required');
 
     state.todos.push(new Todo(description) );
+    saveStateToLocalStorage();
 
 }
 
@@ -72,20 +84,25 @@ const toggleTodo = ( todoId ) => {
         return todo;
     });
 
+    saveStateToLocalStorage();
+
 }
 
 const deleteTodo = ( todoId ) => {
     state.todos = state.todos.filter( todo => todo.id !== todoId );
+    saveStateToLocalStorage();
 
 }
 
 const deleteCompleted = () => {
     state.todos = state.todos.filter( todo => todo.done );
+    saveStateToLocalStorage();
 
 }
 
 const setFilter = ( newFilter = Filters.All ) => {
     state.filter = newFilter;
+    saveStateToLocalStorage();
 }
 
 const getCurrentFilter = () => {
