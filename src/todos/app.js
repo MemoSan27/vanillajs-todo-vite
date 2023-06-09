bc3d8c3e40ba4cb31e0016568d8da5a0dce4f196
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-09 12:00:45 -0700

    app.js addTodo funcionando

--- src/todos/app.js
+++ src/todos/app.js
@@ -5,6 +5,7 @@ import { renderTodos } from './use-cases';
 
 const ElementIDS = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 }
 
 /**
@@ -28,5 +29,20 @@ export const App = ( elementId ) => {
         displayTodos();
         
     })();
+
+
+    //Referencias HTML
+    const newDescriptionInput = document.querySelector( ElementIDS.NewTodoInput );
+
+    //Listeners
+    newDescriptionInput.addEventListener( 'keyup', ( event ) => {
+            if( event.keyCode !== 13) return;
+            if( event.target.value.trim().length === 0 ) return;
+
+            todoStore.addTodo( event.target.value );
+            displayTodos();
+
+            event.target.value = '';
+    });
 
 }
