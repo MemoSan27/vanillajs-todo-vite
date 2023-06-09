15ca185f38b6d7161e923650577425a71cff0f15
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-06-09 10:20:27 -0700

    todo.store.js Metodo getTodos completado

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -30,7 +30,19 @@ const loadStore = () => {
 
 
 const getTodos = ( filter = Filters.All) =>{
+    switch( filter ) {
+        case Filters.All:
+           return [...state.todos];
+           
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
 
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+    }
 }
 
 /**
@@ -76,6 +88,7 @@ export default {
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
